43e7eb6b02d7897e6c7784f23c87213dc5390ca3
Dhruva Bansal <dbansal36@gatech.edu>
2020-05-20 20:40:20 +0530

    Train AdaBoosted Classifiers

--- SequentialClassification/main/projects/RaviSBHMM/trainAdaboostedClassifier.py
+++ SequentialClassification/main/projects/RaviSBHMM/trainAdaboostedClassifier.py
@@ -1,12 +1,13 @@
 from classes import State, Word, Phrase
 from sklearn.ensemble import AdaBoostClassifier
 import sys
+from sklearn.utils import shuffle
 sys.path.insert(0, '../../')
 from src.prepare_data.ark_reader import read_ark_files
 import glob
 import numpy as np
 
-
+random_state = 42
 #structure -> word.name -> index -> state.name -> class
 #supposed to create a map between word, index and the class number
 def getClassTree(phrases):
@@ -50,12 +51,56 @@ def dataSetReader(classLabels, phrases, arkFileLoc):
                 else:
                     dataset[currClass] = content[int(state.start * timeToFrame) : int(state.end * timeToFrame)]
         
-    for dataClass in dataset:
-        print(dataset[dataClass].shape)
-        
+    return dataset
 
-def getTrainedClassifier(phrases):
+def getDataSetForTrainingClass(dataset, currClass):
+    features = []
+    labels = []
+
+    for classLabel in dataset:
+        features.extend(dataset[classLabel])
+        if classLabel == currClass:
+            labels.extend([1 for i in range(dataset[classLabel].shape[0])])
+        else:
+            labels.extend([0 for i in range(dataset[classLabel].shape[0])])
+    
+    return np.array(features), np.array(labels)
+
+
+
+def getTrainedClassifier(phrases, trainMultipleClassifiers=True):
     classLabels = getClassTree(phrases)
-    dataSetReader(classLabels, phrases, "data/ark/")
-    # print(classLabels)
+    dataset = dataSetReader(classLabels, phrases, "data/ark/")
 
+    if trainMultipleClassifiers:
+        classifiers = [AdaBoostClassifier(n_estimators=100, random_state=random_state) for classLabel in dataset]
+
+        for classLabel in dataset:
+            X, Y = getDataSetForTrainingClass(dataset, classLabel)
+            X, Y = shuffle(X, Y, random_state=random_state)
+            classifiers[classLabel].fit(X, Y)
+            # print("Classifier " + str(classLabel) + " accepted training score = " + str(classifiers[classLabel].score(dataset[classLabel], [1 for i in range(len(dataset[classLabel]))])))
+            # print("Number accepted = "+str(len(dataset[classLabel])))
+    
+    else:
+        features = []
+        labels = []
+        for classLabel in dataset:
+            features.extend(dataset[classLabel])
+            labels.extend([classLabel for i in range(dataset[classLabel].shape[0])])
+        features = np.array(features)
+        labels = np.array(labels)
+
+        classifier = AdaBoostClassifier(n_estimators=100, random_state=random_state)
+        classifier.fit(features, labels)
+        
+        for classLabel in dataset:
+            labelDataset = [classLabel for i in range(len(dataset[classLabel]))]
+            print(labelDataset)
+            # print("Classifier " + str(classLabel) + " accepted training score = " + str(classifier.score(dataset[classLabel], [classLabel for i in range(len(dataset[classLabel]))])))
+            # print("Number accepted = "+str(len(dataset[classLabel])))
+
+    
+    
+    return classifiers
+    
