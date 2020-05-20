20df9fae28a642bd4eeeef2115b35258d02c4b6a
Dhruva Bansal <dbansal36@gatech.edu>
2020-05-20 18:55:04 +0530

    Sythesis dataset for training adaboost

--- SequentialClassification/main/projects/RaviSBHMM/trainAdaboostedClassifier.py
+++ SequentialClassification/main/projects/RaviSBHMM/trainAdaboostedClassifier.py
@@ -4,9 +4,10 @@ import sys
 sys.path.insert(0, '../../')
 from src.prepare_data.ark_reader import read_ark_files
 import glob
+import numpy as np
 
 
-#structure -> word -> index -> state
+#structure -> word.name -> index -> state.name -> class
 #supposed to create a map between word, index and the class number
 def getClassTree(phrases):
     wordToDict = {}
@@ -26,19 +27,35 @@ def getClassTree(phrases):
                     currClass += 1
             index += 1
 
-    # print("Total classes = " + str(currClass))
+    print("Total classes = " + str(currClass))
     return wordToDict
 
-def dataSetReader(phrases, arkFileLoc):
+def dataSetReader(classLabels, phrases, arkFileLoc):
+    dataset = {}  ##Class to frames
     for phrase in phrases:
         currPhraseArk = arkFileLoc+phrase.name+".ark"
         content = read_ark_files(currPhraseArk)
-        print(currPhraseArk)
-        print(content.shape[0])
-        print(phrase.end)
-        print(content.shape[0]/phrase.end)
+
+        timeToFrame = content.shape[0]/phrase.end  ##aka frame rate
+
+        for index, word in enumerate(phrase.words):
+            for state in word.states:
+                currClass = classLabels[word.name][index][state.name]
+                
+                # temp = content[int(state.start * timeToFrame) : int(state.end * timeToFrame)]
+                # if temp.shape[0] == 1:
+                #     print(temp.shape)
+                if currClass in dataset:
+                    dataset[currClass] = np.concatenate((dataset[currClass], content[int(state.start * timeToFrame) : int(state.end * timeToFrame)]))
+                else:
+                    dataset[currClass] = content[int(state.start * timeToFrame) : int(state.end * timeToFrame)]
+        
+    for dataClass in dataset:
+        print(dataset[dataClass].shape)
+        
+
 def getTrainedClassifier(phrases):
     classLabels = getClassTree(phrases)
-    dataSetReader(phrases, "data/ark/")
+    dataSetReader(classLabels, phrases, "data/ark/")
     # print(classLabels)
 
