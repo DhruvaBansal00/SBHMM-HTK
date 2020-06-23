0c1fa6615db3b3c3da9db65be1539475d1110229
Dhruva Bansal <dbansal36@gatech.edu>
2020-06-23 14:15:13 +0530

    Added parallelization for training classifiers

--- SequentialClassification/main/src/test/test.py
+++ SequentialClassification/main/src/test/test.py
@@ -52,7 +52,7 @@ def test(start: int, end: int, method: str, insertion_penalty: int) -> None:
     elif method == 'verification':
 
         HVite_str = (f'HVite -a -o N -T 1 -H $macros -S '
-                     f'lists/test.data -i $results -m -y lab -t 250.0 -s 1.0 '
+                     f'lists/test.data -i $results -m -t 250.0 -s 1.0 '
                      f'-p {insertion_penalty} -I all_labels.mlf -s 25 dict wordList')
         HVite_cmd = Template(HVite_str)
         HResults_cmd = Template('')
@@ -60,7 +60,7 @@ def test(start: int, end: int, method: str, insertion_penalty: int) -> None:
     elif method == 'alignment':
 
         HVite_str = (f'HVite -a -o N -T 1 -H $macros -m -f -S '
-                     f'lists/train.data -i $results -m -y lab -t 250.0 -s 1.0 '
+                     f'lists/train.data -i $results -t 250.0 '
                      f'-p {insertion_penalty} -I all_labels.mlf -s 25 dict wordList '
                      f'>/dev/null 2>&1')
         HVite_cmd = Template(HVite_str)
