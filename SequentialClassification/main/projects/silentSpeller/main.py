91386a3c49efe7426c4a1b43ae334b1df0c3916c
Dhruva Bansal <dbansal36@gatech.edu>
2020-07-01 20:11:35 +0530

    Add parallelization to test

--- SequentialClassification/main/projects/silentSpeller/main.py
+++ SequentialClassification/main/projects/silentSpeller/main.py
@@ -103,7 +103,8 @@ if __name__ == '__main__':
             classifiers = trainSBHMM(args.sbhmm_cycles, args.train_iters, args.mean, args.variance, args.transition_prob, args.device, 
                         args.pca_components, args.sbhmm_iters, args.include_word_level_states, args.include_word_position, args.no_pca, 
                         args.insertion_penalty, args.parallel_jobs, args.parallel_classifier_training)
-            testSBHMM(args.start, args.end, args.method, classifiers, args.pca_components, args.no_pca, args.insertion_penalty)
+            testSBHMM(args.start, args.end, args.method, classifiers, args.pca_components, args.no_pca, args.insertion_penalty, 
+                    args.parallel_jobs, args.parallel_classifier_training)
         else:
             train(args.train_iters, args.mean, args.variance, args.transition_prob, args.device)
             test(args.start, args.end, args.method, args.insertion_penalty)
@@ -175,7 +176,8 @@ if __name__ == '__main__':
                 classifiers = trainSBHMM(args.sbhmm_cycles, args.train_iters, args.mean, args.variance, args.transition_prob, args.device,
                             args.pca_components, args.sbhmm_iters, args.include_word_level_states, args.include_word_position, args.no_pca,
                             args.insertion_penalty, args.parallel_jobs, args.parallel_classifier_training)
-                testSBHMM(args.start, args.end, args.method, classifiers, args.pca_components, args.no_pca, args.insertion_penalty)
+                testSBHMM(args.start, args.end, args.method, classifiers, args.pca_components, args.no_pca, args.insertion_penalty, 
+                        args.parallel_jobs, args.parallel_classifier_training)
             else:
                 train(args.train_iters, args.mean, args.variance, args.transition_prob, args.device)
                 test(args.start, args.end, args.method, args.insertion_penalty)
@@ -232,7 +234,8 @@ if __name__ == '__main__':
             classifiers = trainSBHMM(args.sbhmm_cycles, args.train_iters, args.mean, args.variance, args.transition_prob, args.device,
                             args.pca_components, args.sbhmm_iters, args.include_word_level_states, args.include_word_position, 
                             args.no_pca, args.insertion_penalty, args.parallel_jobs, args.parallel_classifier_training)
-            testSBHMM(args.start, args.end, args.method, classifiers, args.pca_components, args.no_pca, args.insertion_penalty)
+            testSBHMM(args.start, args.end, args.method, classifiers, args.pca_components, args.no_pca, args.insertion_penalty, 
+                    args.parallel_jobs, args.parallel_classifier_training)
         else:
             train(args.train_iters, args.mean, args.variance, args.transition_prob, args.device)
             test(args.start, args.end, args.method, args.insertion_penalty)
--- SequentialClassification/main/src/test/testSBHMM.py
+++ SequentialClassification/main/src/test/testSBHMM.py
@@ -18,7 +18,8 @@ from src.prepare_data.ark_reader import read_ark_files
 from src.prepare_data.ark_creation import _create_ark_file
 from src.prepare_data.htk_creation import create_htk_files
 
-def testSBHMM(start: int, end: int, method: str, classifiers: [], pca_components: int, no_pca: bool, insertion_penalty: int) -> None:
+def testSBHMM(start: int, end: int, method: str, classifiers: [], pca_components: int, no_pca: bool, insertion_penalty: int, 
+            parallel: bool, n_jobs: int) -> None:
 
     print("-------Testing SBHMM-----------")
     testDataFile = "lists/test.data"
@@ -48,7 +49,7 @@ def testSBHMM(start: int, end: int, method: str, classifiers: [], pca_components
         content = read_ark_files(arkFile)
         newContent = content
         for classifier in classifiers:
-            newContent = classifier.getTransformedFeatures(newContent)
+            newContent = classifier.getTransformedFeatures(newContent, parallel, n_jobs)
             if not no_pca:
                 pca = PCA(n_components=pca_components)
                 newContent = pca.fit_transform(newContent)
