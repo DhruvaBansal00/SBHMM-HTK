cb5d2f067344a4f67052a397571a6c8d2f17d742
Dhruva Bansal <dbansal36@gatech.edu>
2020-07-25 14:03:37 +0530

    Initialize exp with new dataset

--- SilentSpellerDR/createArkHtk.py
+++ SilentSpellerDR/createArkHtk.py
@@ -92,6 +92,6 @@ def createMLF(htk_dir: str, mlf_file: str) -> None:
 
 if __name__ == "__main__":
 
-    createArk('/home/dhruva/Desktop/CopyCat/SilentSpeller/PCA/naoki_pca/16/*', '/home/dhruva/Desktop/CopyCat/SilentSpeller/PCA/naoki_pca/label/*', "/home/dhruva/Desktop/CopyCat/SilentSpeller/data/ark/")
+    createArk('/home/dhruva/Desktop/CopyCat/SilentSpeller/gt2k_2328_16/data/*', '/home/dhruva/Desktop/CopyCat/SilentSpeller/gt2k_2328_16/label/*', "/home/dhruva/Desktop/CopyCat/SilentSpeller/data/ark/")
     createHTK('/home/dhruva/Desktop/CopyCat/SilentSpeller/data/ark/*', '/home/dhruva/Desktop/CopyCat/SilentSpeller/data/htk/')
     createMLF('/home/dhruva/Desktop/CopyCat/SilentSpeller/data/htk/*', '/home/dhruva/Desktop/CopyCat/SilentSpeller/all_labels.mlf')
