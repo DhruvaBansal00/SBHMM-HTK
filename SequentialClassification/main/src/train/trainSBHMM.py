7f1a78d7d68c5c5fcf74eac04f423172ed29bf97
Dhruva Bansal <dbansal36@gatech.edu>
2020-07-27 13:07:01 +0530

    hotfix for number of neighbors

--- SequentialClassification/main/src/train/trainSBHMM.py
+++ SequentialClassification/main/src/train/trainSBHMM.py
@@ -44,7 +44,7 @@ def createNewArkFile(arkFile: str, trainedClassifier: object, pca_components: in
 def trainSBHMM(sbhmm_cycles: int, train_iters: list, mean: float, variance: float, 
             transition_prob: float, device: int, pca_components: int, sbhmm_iters: list, 
             include_state: bool, include_index: bool, no_pca: bool, hmm_insertion_penalty: float, sbhmm_insertion_penalty: float,
-            n_jobs: int, parallel: bool, trainMultipleClassifiers: bool, knn_neighbors: int, beam_threshold: float) -> object:
+            n_jobs: int, parallel: bool, trainMultipleClassifiers: bool, knn_neighbors: str, beam_threshold: float) -> object:
     """Trains the SBHMM using HTK. First completes a loop of
     training HMM as usual. Then completes as many iterations of 
     KNN + HMM training as specified.
@@ -69,7 +69,7 @@ def trainSBHMM(sbhmm_cycles: int, train_iters: list, mean: float, variance: floa
 
         trainedClassifier = getClassifierFromStateAlignment(resultFile, arkFileLoc, include_state=include_state, 
                         include_index=include_index, n_jobs=n_jobs, parallel=parallel, trainMultipleClassifiers=trainMultipleClassifiers,
-                        knn_neighbors=knn_neighbors)
+                        knn_neighbors=int(knn_neighbors))
         classifiers.append(trainedClassifier)
         
         arkFileSave = "data/arkSBHMM"+str(iters)+"/"
