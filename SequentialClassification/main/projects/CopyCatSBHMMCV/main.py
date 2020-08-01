9d8a5403513b4ca7808648f9e636e6e1d551d734
pranay <pagrawal78@gatech.edu>
2020-08-01 15:13:04 -0400

    SOTA results on Prerna + Linda + Matthew

--- SequentialClassification/main/projects/CopyCatSBHMMCV/main.py
+++ SequentialClassification/main/projects/CopyCatSBHMMCV/main.py
@@ -1,8 +1,9 @@
 """Main file used to prepare training data, train, and test HMMs.
-    HMM EX = python3 main.py --test_type standard --train_iters 25 50 75 100 --users Prerna Linda | python3 main.py --test_type cross_val --train_iters 25 50 75 100 --users Prerna Linda --cross_val_method stratified --n_splits 10 --cv_parallel --parallel_jobs 4  --hmm_insertion_penalty -80
+    HMM EX = python3 main.py --test_type standard --train_iters 25 50 75 100 --users Prerna Linda | 
+    HMM CV = python3 main.py --test_type cross_val --train_iters 25 50 75 100 120 140 160 --users 02-22-20_Prerna_Android 04-29-20_Linda_Android 07-24-20_Matthew_4K --cross_val_method stratified --n_splits 10 --cv_parallel --parallel_jobs 10  --hmm_insertion_penalty -80
     SBHMM EX = python3 main.py --test_type standard --train_iters 25 50 75 --sbhmm_iters 25 50 75 --users Prerna Linda --train_sbhmm --sbhmm_cycles 1 --no_pca --include_word_level_states --include_word_position --parallel_classifier_training --parallel_jobs 4 --hmm_insertion_penalty -70 --sbhmm_insertion_penalty -115 --neighbors 70
     SBHMM CV = python3 main.py --test_type cross_val --train_iters 25 50 75 --sbhmm_iters 25 50 75 --users Linda Prerna --train_sbhmm --sbhmm_cycles 1 --no_pca --include_word_level_states --include_word_position --parallel_classifier_training --parallel_jobs 4 --hmm_insertion_penalty -85 --sbhmm_insertion_penalty -85 --neighbors 70 --cross_val_method kfold --n_splits 10 --beam_threshold 2000.0
-    SBHMM CV Parallel = python3 main.py --test_type cross_val --train_iters 25 50 75 100 120 --sbhmm_iters 25 50 75 --users 02-22-20_Prerna_Android 04-29-20_Linda_Android 07-24-20_Matthew_4K --train_sbhmm --sbhmm_cycles 1 --no_pca --include_word_level_states --include_word_position --parallel_classifier_training --hmm_insertion_penalty -80 --sbhmm_insertion_penalty -100 --neighbors 73 --cross_val_method stratified --n_splits 10 --beam_threshold 3000.0 --cv_parallel --parallel_jobs 4
+    SBHMM CV Parallel = python3 main.py --test_type cross_val --train_iters 25 50 75 100 120 140 160 --sbhmm_iters 25 50 75 100 --users 02-22-20_Prerna_Android 04-29-20_Linda_Android 07-24-20_Matthew_4K --train_sbhmm --sbhmm_cycles 1 --no_pca --include_word_level_states --include_word_position --parallel_classifier_training --hmm_insertion_penalty -80 --sbhmm_insertion_penalty -80 --neighbors 70 --cross_val_method stratified --n_splits 10 --beam_threshold 3000.0 --cv_parallel --parallel_jobs 10
 """
 import sys
 import glob
@@ -31,7 +32,7 @@ def copyFiles(fileNames: list, newFolder: str, originalFolder: str, ext: str):
         shutil.copyfile(os.path.join(originalFolder, currFile+ext), os.path.join(newFolder, currFile+ext))
 
 def crossValFold(train_data: list, test_data: list, args: object, fold: int):
-    print(f"Current split = {str(fold)}")
+    print(f"Current split = {str(fold)}. Current Test data Size = {len(test_data)}")
     ogDataFolder = "data"
     currDataFolder = os.path.join("data", str(fold))
     trainFiles = [i.split("/")[-1].strip(".htk") for i in train_data]
