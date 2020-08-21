d5171d4ae3b5c2e625e92e4fb679d076f439922a
Dhruva Bansal <dbansal36@gatech.edu>
2020-08-21 15:43:00 -0400

    Perform alignment at then end of each fold

--- SequentialClassification/main/src/main.py
+++ SequentialClassification/main/src/main.py
@@ -74,6 +74,8 @@ def crossValFold(train_data: list, test_data: list, args: object, fold: int):
     print(f'Current Insertion Error: {results["insertions"]}')
     print(f'Current Deletions Error: {results["deletions"]}')
 
+    test(-1, -1, "alignment", args.hmm_insertion_penalty, beam_threshold=args.beam_threshold, fold=os.path.join("alignment", str(fold), ""))
+
     return [results['error'], results['sentence_error'], results['insertions'], results['deletions']]
 
     
