35849f0ba65dc723fa7724b572599de750a5032d
Dhruva Bansal <dhruva.bansal00@gmail.com>
2020-10-05 12:59:56 -0400

    exp 123

--- SequentialClassification/main/projects/Kinect/configs/config_kinect_features.py
+++ SequentialClassification/main/projects/Kinect/configs/config_kinect_features.py
@@ -27,7 +27,7 @@ def feature_selection():
     delta_relative_to_nose = [] #[f'delta_{feature}_to_nose_{coordinate}' for coordinate in coordinates]
     
     standardized_no_squared_positions = [] #[f'standardized_{feature}_{coordinate}' for coordinate in coordinates]
-    standardized_squared_positions = [] #[f'standardized_{feature}_squared_{coordinate}' for coordinate in coordinates]
+    standardized_squared_positions = [f'standardized_{feature}_squared_{coordinate}' for coordinate in coordinates]
 
     quantile_no_squared_positions = []#[f'quantile_{feature}_{coordinate}' for coordinate in coordinates]
     quantile_squared_positions = [f'quantile_{feature}_squared_{coordinate}' for coordinate in coordinates]
