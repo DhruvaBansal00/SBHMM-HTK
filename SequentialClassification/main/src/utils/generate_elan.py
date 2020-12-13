7e8abdbb1b4628a85f8c02ed58cd70990ceefc46
Ishan <ishanchadha01@gmail.com>
2020-12-13 12:58:59 -0500

    organized elan script

--- SequentialClassification/main/src/utils/generate_elan.py
+++ SequentialClassification/main/src/utils/generate_elan.py
@@ -5,8 +5,110 @@ import glob
 from pympi.Elan import Eaf, to_eaf
 
 
+def is_file_name(name: str) -> bool:
+    return len(name)>0 and name.endswith("\"") and name[0]=="\""
+
+
+def mlf_to_dict(mlf_filepath: str):
+    '''Generates dictionary from mlf file
+
+    Parameters
+    ----------
+    eaf_filepath : str
+        File path at which mlf file is located.
+
+    Returns dictionary with this format:
+    {filename : 
+        {word : 
+            [
+                [state, start, end]
+                ...
+            ]
+        }
+        ...
+    ...
+    }
+    '''
+    out_dict = {}
+
+    # Iterate over lines of mlf file
+    with open(mlf_filepath, "rb") as mlf:
+        out_path = None
+        header = mlf.readline()
+        lines = mlf.readlines()
+        line_num = 0
+        for line in lines:
+            line = line.decode('utf-8').strip()
+
+            # If line is file name, add new entry in dictionary
+            if is_file_name(line):
+                fname = '.'.join(line.split('/')[-1].split('.')[:-1])
+                out_dict[fname] = {}
+
+            # If line has state and boundary data
+            elif line != '.':
+                line_arr = line.split(" ")
+                if len(line_arr) >= 5:
+                    word = line_arr[4]
+                    out_dict[fname][word] = []
+                state = line_arr[2]
+                start = int(line_arr[0])/1000
+                end = int(line_arr[1])/1000
+                out_dict[fname][word].append([state, start, end])
+        
+        return out_dict
+        
+def make_elan(data: dict, has_states: bool, video_dirs: list, eaf_savedir: str) -> None:
+    """Generates eaf files from data dict
+
+    Parameters
+    ----------
+    data : dict
+        Segmentation data extracted from mlf files.
+
+    has_states : bool
+        Whether or not to write individual states to eaf.
+
+    video_dirs : list[str]
+        List of videos to create eaf objects with.
+
+    eaf_savedir : str
+        Directory under which eaf files are saved.
+    """
+
+    video_names = [ vname.split('/')[-1][:-4] for vname in video_dirs ]
+
+    for fname in data:
+        if fname in video_names:
+            video_fp = video_dirs[video_names.index(fname)]
+
+            out_path = os.path.join(eaf_savedir, fname + '.eaf')
+
+            # Create base eaf file
+            shutil.copy("elan.txt", out_path)
+
+            # Create eaf object and link video
+            eaf_file = Eaf(out_path)
+            eaf_file.add_linked_file(video_fp.replace(' ', '\ '), mimetype="video/mp4")
+
+            # Iterate over segmentation data
+            if not has_states:
+                eaf_file.add_tier(fname)
+                for word in fname:
+                    eaf_file.add_annotation(fname, word[0][1], word[-1][2])
+            else:
+                for word in fname:
+                    eaf_file.add_tier(word)
+                    for state in word:
+                        eaf_file.add_annotation(word, state[1], state[2])
+            
+            # Create eaf out of data
+            to_eaf(out_path, eaf_file)
+
+
+
 def mlf_to_elan(mlf_filepath: str, video_dirs: list, eaf_savedir: str) -> None:
-    """Generates csv files from mlf file
+    """Generates eaf files from mlf file
 
     Parameters
     ----------
@@ -30,7 +132,6 @@ def mlf_to_elan(mlf_filepath: str, video_dirs: list, eaf_savedir: str) -> None:
         line_num = 1
         while line_num < len(lines):
             line = str(lines[line_num])
-            print(line)
             updated = False
 
             if len(line) < 10:
@@ -45,7 +146,6 @@ def mlf_to_elan(mlf_filepath: str, video_dirs: list, eaf_savedir: str) -> None:
                     to_eaf(out_path, eaf_file)
 
                 # create filename out of header info
-                print(line)
                 fname = line.split('/')[-1][:-8]
 
                 # take eaf_savedir and append filename to create out_path
@@ -88,6 +188,7 @@ def mlf_to_elan(mlf_filepath: str, video_dirs: list, eaf_savedir: str) -> None:
         if eaf_file:
             to_eaf(out_path, eaf_file)
 
+
 if __name__=='__main__':
 
     # Find where videos are located on desktop
@@ -95,7 +196,7 @@ if __name__=='__main__':
 
     # Save annotated videos on desktop
     save_dir = '/media/thad/Seagate Backup Plus Drive/video_annotation'
-
+    '''
     # Iterate over MLF files
     results = '../../projects/Kinect/results/'
     mlf_dirs = [ os.path.join(results, mlf) for mlf in os.listdir(results) if os.path.isdir(os.path.join(results, mlf)) ]
@@ -103,3 +204,9 @@ if __name__=='__main__':
         for mlf in os.listdir(mlf_dir):
             print(mlf_dir)
             mlf_to_elan(os.path.join(mlf_dir, mlf), video_dirs, save_dir)
+    '''
+    results = '../../projects/Kinect/results/'
+    for mlf_dir in os.listdir(results):
+        for mlf in os.listdir(os.path.join(results, mlf_dir)):
+            data = mlf_to_dict(os.path.join(results, mlf_dir, mlf))
+            make_elan(data, False, video_dirs, save_dir)
